2b2069d17cad0a4bbe27c2454f662f6ae487eedd
miscalculation53 <50172036+miscalculation53@users.noreply.github.com>
2024-08-26 21:07:06 +0900

    Delete mclib directory

--- mclib/template/template_all.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "template_types.hpp"
-#include "template_rep.hpp"
-#include "template_math.hpp"
-#include "template_vector.hpp"
-#include "template_bit.hpp"
-#include "template_inout.hpp"
-#include "template_dump.hpp"
--- mclib/template/template_dump.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifdef LOCAL
-  #include <cpp-dump.hpp> // https://github.com/philip82148/cpp-dump
-  #define dump(...) cpp_dump(__VA_ARGS__)
-#else
-  #define dump(...)
-#endif
