dacfd12eddd90b50e5a32edf107c923803642415
miscalculation53 <seesawseen20@gmail.com>
2025-03-21 00:46:29 +0900

    flat_map のテストを追加

---
+++ verify/yosupo/associative_array_flat_map.test.cpp
@@ -0,0 +1,104 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#define SINGLE_TESTCASE
+// #define MULTI_TESTCASE
+// #define AOJ_TESTCASE
+
+#define FAST_IO
+// #define FAST_CIO
+// #define INTERACTIVE
+
+#define INF 4'000'000'000'000'000'037LL
+#define EPS 1e-11
+
+#include "template/template_all.hpp"
+
+#include "ds/flat_map.hpp"
+
+void init() {}
+
+void main2()
+{
+  LL(Q);
+  vc<tlll> queries(Q);
+  vc<ll> keys;
+  rep(i, Q)
+  {
+    LL(t);
+    if (t == 0)
+    {
+      LL(k, v);
+      queries.at(i) = {t, k, v};
+      keys.eb(k);
+    }
+    else if (t == 1)
+    {
+      LL(k);
+      queries.at(i) = {t, k, -1};
+      keys.eb(k);
+    }
+  }
+  FlatMap<ll, ll> mp(keys);
+  fec([ key, val ] : mp) key + val;
+  fec([t, k, v] : queries)
+  {
+    if (t == 0)
+    {
+      mp[k] = v;
+    }
+    else if (t == 1)
+    {
+      PRINT(mp[k]);
+    }
+  }
+}
+
+void test() {}
+
+int main()
+{
+  cauto CERR = [](string val, string color)
+  {
+    string s = "\033[" + color + "m" + val + "\033[m";
+    #ifdef LOCAL
+    cerr << s;
+    #endif
+    /* コードテストで確認する際にコメントアウトを外す
+    cerr << val;
+    //*/
+  };
+
+  #if defined FAST_IO and not defined LOCAL
+  CERR("\n[FAST_IO]\n\n", "32");
+  #endif
+  #if defined FAST_CIO and not defined LOCAL
+  CERR("\n[FAST_CIO]\n\n", "32");
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  #endif
+  cout << fixed << setprecision(20);
+
+  test();
+  init();
+
+  #if defined AOJ_TESTCASE or (defined LOCAL and defined SINGLE_TESTCASE)
+  CERR("\n[AOJ_TESTCASE]\n\n", "35");
+  while (true)
+  {
+    dump("new testcase");
+    main2();
+  }
+  #elif defined SINGLE_TESTCASE
+  CERR("\n[SINGLE_TESTCASE]\n\n", "36");
+  main2();
+  #elif defined MULTI_TESTCASE
+  CERR("\n[MULTI_TESTCASE]\n\n", "33");
+  dump("T");
+  IN(uint, T);
+  while (T--)
+  {
+    dump("new testcase");
+    main2();
+  }
+  #endif
+}
